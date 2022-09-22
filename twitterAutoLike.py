60b11432b468ada7467621f0720fceb7c04c0e61
azkatang <mog2to4aki@gmail.com>
2022-09-22 21:03:07 +0900

    update twitterAutoLike.py

--- twitterAutoLike.py
+++ twitterAutoLike.py
@@ -1,3 +1,25 @@
 import tweepy
 
 APIKEY = ""
+APIKEYSECRET = ""
+ACCESSTOKEN = ""
+ACCESSTOKENSECRET = ""
+
+""" 認証準備 """
+auth = tweepy.OAuthHandler(APIKEY, APIKEYSECRET)
+auth.set_access_token(ACCESSTOKEN, ACCESSTOKENSECRET)
+
+api = tweepy.API(auth)
+
+""" Tweet取得 """
+tweets = api.search_tweets(q="プログラミング", result_type="recent", count=5)
+#print(objects)
+
+for tweet in tweets:
+    try:
+        favoriteId = tweet.id
+        api.create_favorite(favoriteId)
+        print("Like text is: " + tweet.text)
+
+    except tweepy.TweepyException as e:
+        print(e)
